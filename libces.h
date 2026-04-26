f6ef3c542d5366d3049a73e92be417750ea095cb
gloader <ascaniofrancys483@gmail.com>
2026-04-25 23:30:00 -0400

    linces.h
    C en español

---
+++ libces.h
@@ -0,0 +1,30 @@
+#ifndef LIBES_H
+#define LIBES_H
+#define devolver return
+#define si if
+#define sino else
+#define entero int
+#define constante const
+#define flotar float
+#define doble double
+#define caracter char
+#define vacio void
+#define mientras while
+#define para for
+#define hacer do
+#define caso case
+#define elegir switch
+#define continuar continue
+#define romper break
+#define principal main
+#define estructura struct
+#define tipodef typedef
+#define externo extern
+#define estatico static
+#define enumerar enum
+#define largo long
+#define corto short
+#define nosigno unsigne
+#define signo signe
+#define defecto default
+#endif
